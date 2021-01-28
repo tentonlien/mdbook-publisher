955b694e5a64c3c9a4e46d2ba22dce7125d14b71
Tenton Lien <tentonlien@users.noreply.github.com>
2021-01-28 20:30:59 +0800

    Support uploading to target host

--- mdpub.py
+++ mdpub.py
@@ -6,26 +6,68 @@ import json
 import os
 import sys
 
+# Global config
+config = {}
+
 def generate(name, path):
     with open(path, 'r', encoding='utf-8') as f:
-        content = f.read()
+        summary_text = '# Summary\n\n'
+        item_array = f.read().split('\n- ')
+        print(str(len(item_array) - 1) + ' item(s) found.')
+        
+        content = None
+        for item in item_array:
+            if content == None:
+                content = item
+            else:
+                content = content + '\n\n- ' + item
+
+        # Generate single content file
         arr = content.split('\n# ')
+        if arr[0].find('# ') == 0:
+            arr[0] = arr[0][2:]
+
         for elem in arr:
             title_pos_start = title_pos_end = 0
             while elem[title_pos_end] != '\n' and title_pos_end < len(elem) - 1:
                 title_pos_end = title_pos_end + 1
 
-            title = elem[title_pos_start:title_pos_end]
+            title = elem[title_pos_start : title_pos_end]
             with open('projects/' + name + '/src/' + title + '.md', 'w', encoding='utf-8') as fout:
-                fout.write(elem[title_pos_end:len(elem)])
+                fout.write('# ' + title + '\n' + elem[title_pos_end:len(elem)])
                 fout.close()
+            
+            summary_text = summary_text + '- [' + title + '](./' + title + '.md)\n'
+        
+        # Generate SUMMARY.md
+        with open('projects/' + name + '/src/SUMMARY.md', 'w', encoding='utf-8') as fout:
+            fout.write(summary_text)
+            fout.close()
                 
     print('Generate success')
+
+    os.system('mdbook build projects/' + name)
+
+
+def upload(name, target, dest):
+    target_host_info = {}
+
+    for host in config['hosts']:
+        if host['name'] == target:
+            target_host_info = host
+            break
+    
+    if target_host_info == {}:
+        print('Error: Target host not found')
+        exit()
+
+    cmd = 'scp -r ./projects/' + name + '/book/* ' + target_host_info['username'] + '@' + target_host_info['address'] + ':' + dest
+    os.system(cmd)
 
 
 def main():
     # Load config
-    config = {}
+    global config
     with open('mdpub.json', 'r', encoding='utf-8') as f:
         config = json.load(f)
 
@@ -49,7 +91,7 @@ def main():
             print('Missing argument')
 
     elif len(sys.argv) == 3:
-        if sys.argv[1] == 'gen':
+        if sys.argv[1] == 'gen' or sys.argv[1] == 'update':
             project_name = sys.argv[2]
 
             # Create if project directory not exists
@@ -59,18 +101,25 @@ def main():
 
             # Intialize project
             if (not os.path.exists('projects/' + project_name)):
-                os.mkdir('projects/' + prject_name)
+                os.mkdir('projects/' + project_name)
                 os.system('mdbook init projects/' + project_name)
     
             # Fetch soruce markdown file path
             src_path = ''
+            target_host = ''
+            dest_path = ''
             for project in config['projects']:
                 if (project['name'] == project_name):
                     src_path = project['src']
+                    target_host = project['host']
+                    dest_path = project['dest']
                     break
     
             if src_path != '':
                 generate(project_name, src_path)
+                
+                if sys.argv[1] == 'update':
+                    upload(project_name, target_host, dest_path)
 
 
 if __name__ == '__main__':
